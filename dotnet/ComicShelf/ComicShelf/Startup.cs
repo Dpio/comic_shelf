a63abcba9d9766b0bd5f8fb5d1d0cb1f61cbd4d2
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-02-01 22:03:21 +0100

    Added swagger

--- dotnet/ComicShelf/ComicShelf/Startup.cs
+++ dotnet/ComicShelf/ComicShelf/Startup.cs
@@ -1,3 +1,4 @@
+using ComicShelf.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,13 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ComicShelf
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
+		private readonly IHostingEnvironment _env;
+
+		public Startup(IConfiguration configuration, IHostingEnvironment env)
 		{
+			_env = env;
 			Configuration = configuration;
 		}
 
@@ -27,6 +32,16 @@ namespace ComicShelf
 			{
 				configuration.RootPath = "ClientApp/dist";
 			});
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info
+				{
+					Version = "v1",
+					Title = "API"
+				});
+			});
+			services.AddMvc();
+			IocModule.RegisterDependencies(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,29 +57,37 @@ namespace ComicShelf
 				app.UseHsts();
 			}
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseSpaStaticFiles();
+			//app.UseHttpsRedirection();
+			//app.UseStaticFiles();
+			//app.UseSpaStaticFiles();
 
-			app.UseMvc(routes =>
+			//app.UseMvc(routes =>
+			//{
+			//	routes.MapRoute(
+			//		name: "default",
+			//		template: "{controller}/{action=Index}/{id?}");
+			//});
+
+			//app.UseSpa(spa =>
+			//{
+			//	// To learn more about options for serving an Angular SPA from ASP.NET Core,
+			//	// see https://go.microsoft.com/fwlink/?linkid=864501
+
+			//	spa.Options.SourcePath = "ClientApp";
+
+			//	if (env.IsDevelopment())
+			//	{
+			//		spa.UseAngularCliServer(npmScript: "start");
+			//	}
+			//});
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
 			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller}/{action=Index}/{id?}");
+
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
 			});
-
-			app.UseSpa(spa =>
-			{
-				// To learn more about options for serving an Angular SPA from ASP.NET Core,
-				// see https://go.microsoft.com/fwlink/?linkid=864501
-
-				spa.Options.SourcePath = "ClientApp";
-
-				if (env.IsDevelopment())
-				{
-					spa.UseAngularCliServer(npmScript: "start");
-				}
-			});
+			app.UseMvc();
 		}
 	}
 }
