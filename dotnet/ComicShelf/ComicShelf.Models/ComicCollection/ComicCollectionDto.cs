050a829e531c6e988f44375115ef66ae8343cd6f
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-02-02 01:19:41 +0100

    Added Dtos

---
+++ dotnet/ComicShelf/ComicShelf.Models/ComicCollection/ComicCollectionDto.cs
@@ -0,0 +1,12 @@
+﻿using ComicShelf.Models.Base;
+
+namespace ComicShelf.Models.ComicCollection
+{
+	public class ComicCollectionDto : EntityDto
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int ComicID { get; set; }
+		public int UserId { get; set; }
+	}
+}
