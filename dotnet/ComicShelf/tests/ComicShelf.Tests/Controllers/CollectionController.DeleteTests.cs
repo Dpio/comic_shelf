3f4ceb71482532d02ce255491935820f1168d795
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-02-13 17:25:55 +0100

    Added CollectionController tests

---
+++ dotnet/ComicShelf/tests/ComicShelf.Tests/Controllers/CollectionController.DeleteTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using ComicShelf.DataAccess.Entities;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ComicShelf.Tests.Controllers
+{
+	public class CollectionController_DeleteTests : BaseTestController
+	{
+		[Fact]
+		public async Task ShouldDeleteCollection()
+		{
+			var expected = Fixture.Build<Collection>().With(t => t.Id, 1).Create();
+			Context.Collections.Add(expected);
+			Context.SaveChanges();
+
+			// Act
+
+			var id = 1;
+			var response = await Client.DeleteAsync($"api/Collection/{id}");
+			response.EnsureSuccessStatusCode();
+
+			// Assert
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			var check = await Client.GetAsync($"api/Collection/{id}");
+			Assert.Equal(HttpStatusCode.NotFound, check.StatusCode);
+			var dbTest = Context.Comics.FirstOrDefault(c => c.Id == id);
+			Assert.Null(dbTest);
+		}
+	}
+}
---
+++ dotnet/ComicShelf/tests/ComicShelf.Tests/Controllers/CollectionController.PutTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoFixture;
+using ComicShelf.DataAccess.Entities;
+using ComicShelf.Models.Comic;
+using ComicShelf.Tests.Utilities;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ComicShelf.Tests.Controllers
+{
+	public class CollectionController_PutTests : BaseTestController
+	{
+		[Fact]
+		public async Task ShouldUpdateCollection()
+		{
+			var expected = Fixture.Build<Collection>().With(t => t.Id, 1).Create();
+			Context.Collections.Add(expected);
+			Context.SaveChanges();
+
+			// Act
+			expected.Name = "New Name";
+			expected.Description = "New Description";
+			expected.IsPublic = false;
+			var response = await Client.PutAsync($"api/Collection", new JsonContent(expected));
+			response.EnsureSuccessStatusCode();
+
+			// Assert
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			string jsonResult = await response.Content.ReadAsStringAsync();
+			var deserializedResponse = JsonConvert.DeserializeObject<ComicDto>(jsonResult);
+			var actual = Context.Collections.Find(deserializedResponse.Id);
+			Assert.Equal(expected.Id, actual.Id);
+			Assert.Equal(expected.Name, actual.Name);
+			Assert.Equal(expected.Description, actual.Description);
+			Assert.Equal(expected.IsPublic, actual.IsPublic);
+		}
+	}
+}
