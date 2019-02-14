4dda9898dc58fd7f36aec9a6a997820349098dcf
Piotr Rutkowski <piorutko@gmail.com>
2019-02-14 15:14:38 +0100

    Fix test

--- dotnet/ComicShelf/tests/ComicShelf.Tests/Controllers/ComicCollectionController.GetTest.cs
+++ dotnet/ComicShelf/tests/ComicShelf.Tests/Controllers/ComicCollectionController.GetTest.cs
@@ -16,16 +16,19 @@ namespace ComicShelf.Tests.Controllers
 		[Fact]
 		public async Task ShouldGetComicCollection()
 		{
-			var id = Fixture.Create<int>();
-			var comicCollections = Fixture.Build<ComicCollection>().CreateMany(5).OrderBy(p => p.Id).ToList();
-			var comicCollection = Fixture.Build<ComicCollection>().With(t => t.UserId, id).Create();
+		    var user = Fixture.Build<User>().Create();
+            var comics = Fixture.Build<Comic>().Create();
+            Context.Users.Add(user);
+            Context.Comics.Add(comics);
+		    Context.SaveChanges();
+		    var comicCollections = Fixture.Build<ComicCollection>().With(c => c.ComicId, comics.Id).CreateMany(5).OrderBy(p => p.Id).ToList();
+			var comicCollection = Fixture.Build<ComicCollection>().With(t => t.UserId, user.Id).With(t => t.ComicId, comics.Id).Create();
 			Context.ComicCollections.Add(comicCollection);
 			Context.ComicCollections.AddRange(comicCollections);
 			Context.SaveChanges();
 
 			// Act
-
-			var response = await Client.GetAsync($"api/ComicCollection/getComicsCollection/{id}");
+			var response = await Client.GetAsync($"api/ComicCollection/getComicsCollection/{user.Id}");
 			response.EnsureSuccessStatusCode();
 
 			// Assert
